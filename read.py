d4f83a390490b08259ca3010b8c3e8869b7a88a6
Tina Chen <yunchu101@gmail.com>
2018-06-15 21:26:18 +0800

    calc

--- read.py
+++ read.py
@@ -7,4 +7,11 @@ with open('reviews.txt','r') as f:
 		count += 1 #count = count+1
 		if count % 1000 == 0 :
 			print(len(data))
-	
+print ('finish reading total are', len(data),'line data')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len+len(d)
+print(sum_len)
+print(sum_len/len(data))
+
